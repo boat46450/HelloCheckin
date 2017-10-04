972541f32c41c6fb43ab41b2467b072aa5345424
boat46450 <boat46450@hotmail.com>
2017-10-05 00:08:36 +0700

    moke data in seed

--- database/seeds/CheckinsTableSeeder.php
+++ database/seeds/CheckinsTableSeeder.php
@@ -11,6 +11,19 @@ class CheckinsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('checkins')->insert([
+            'id' => '',
+            'name' => '',
+            'surname' => '',
+            '10-OCT' => false,
+            '12-OCT' => false,
+            '17-OCT' => false,
+            '19-OCT' => false,
+            '24-OCT' => false,
+            '26-OCT' => false,
+            '31-OCT' => false,
+            '02-NOV' => false,
+            '06-NOV' => false,
+        ]);
     }
 }
