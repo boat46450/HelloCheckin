afbe1bccc4c121b9a160025527327056c0a6a304
boat46450 <boat46450@hotmail.com>
2017-10-05 22:43:42 +0700

    add return view

--- app/Http/Controllers/CheckinController.php
+++ app/Http/Controllers/CheckinController.php
@@ -17,6 +17,52 @@ class CheckinController extends Controller
         return view('index');
     }
 
+    // function enterCode
+
+    public function checkinPage() {
+        return view('checkin');
+    }
+
+    // function checkin
+
+    public function exportPage() {
+        $data = [
+            (object)[
+                '_id' => '59130500000',
+                'name' => 'qwe',
+                'surname' => 'zxc',
+                'code' => '456798',
+                'OCT10' => 0,
+                'OCT12' => 0,
+                'OCT17' => 0,
+                'OCT19' => 0,
+                'OCT24' => 0,
+                'OCT26' => 0,
+                'OCT31' => 0,
+                'NOV02' => 0,
+                'NOV06' => 0
+            ],
+            (object)[
+                '_id' => '59130500001',
+                'name' => 'dgh',
+                'surname' => 'uty',
+                'code' => '456790',
+                'OCT10' => 0,
+                'OCT12' => 0,
+                'OCT17' => 0,
+                'OCT19' => 0,
+                'OCT24' => 0,
+                'OCT26' => 0,
+                'OCT31' => 0,
+                'NOV02' => 0,
+                'NOV06' => 0
+            ]
+        ];
+        return view('export', ['data' => $data]);
+    }
+
+    // function export
+
     public function test() {
         dd($this->checkin->get());
     }
