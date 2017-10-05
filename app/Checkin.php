1faa4df13b8c62929753d3aa28e286521df1309f
boat46450 <boat46450@hotmail.com>
2017-10-05 20:46:33 +0700

    edit model

--- app/Checkin.php
+++ app/Checkin.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Checkin extends Model
 {
+    protected $table = 'checkins';
     protected $fillable = [
         'id', 'name', 'surname', 'code', '10-OCT', '12-OCT', '17-OCT', '19-OCT', '24-OCT', '26-OCT', '31-OCT', '02-NOV', '06-NOV'
     ];
